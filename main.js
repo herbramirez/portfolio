977081a7f690e5f98a94b50ee0b0965eb088d077
Herb <herb@Herbs-MacBook-Pro.local>
2023-10-25 14:38:59 -0700

    Updated content across pages with placeholder text and imagery. Included block stylying for animation, text, and gallery. Continued to explore GSAP integration.]

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const tl = gsap.timeline ({
 
 })
 
-gsap.fromTo(".overlay", {x: 1000},{x:0, duration: .5})
+gsap.fromTo(".overlay", {x: 1000},{x:50, duration: 1.5})
 
 gsap.fromTo(".main-container", {width: "0vw", },{width:"90vw", duration: .5})
-gsap.fromTo(".videoPlayer", {opacity: 0},{opacity: 1, duration: 1.5})
+gsap.fromTo(".videoPlayer", {opacity: 0},{opacity: 1, duration: 1.75})
 
