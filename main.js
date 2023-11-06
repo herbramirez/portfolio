8db78f6cc2957eea2844966701a72b1f811489a9
Herb <herb@Herbs-MacBook-Pro.local>
2023-11-06 15:23:05 -0800

    updated Gsap properties for gallery

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-const tl = gsap.timeline ({
+const container = document.querySelector(".gallery-content");
+const items = document.querySelector(".projectLink");
+const videoPlayer = document.querySelector(".videoPlayer");
+const tl = new TimelineMax();
+
+tl.fromTo(container, .75, { width: "0%"}, {width:"100%", ease: "power1.out"});
+
+tl.fromTo(".projectLink", 
+     /*Start  */   { y: 50, opacity: 0, stagger: 0.1,},
+     /*Endt  */ { y:0, duration: 1, opacity: 1, stagger: 0.1}
+  
+  );
 
 
-})
 
+//gsap.fromTo(".overlay", {x: 1000},{x:50, duration: 1.5})
 
-gsap.fromTo(".overlay", {x: 1000},{x:50, duration: 1.5})
-
-gsap.fromTo(".main-container", {width: "0vw", },{width:"90vw", duration: .5})
-gsap.fromTo(".videoPlayer", {opacity: 0},{opacity: 1, duration: 1.75})
+//gsap.fromTo(".gallery-content", {width: "0vw", },{width:"90vw", duration: .75})
+//gsap.fromTo(".gallery-row", {opacity: 0 },{opacity: 1, duration: 1.75})
+//gsap.fromTo(".projectLink", {opacity: 0 },{opacity: 1, duration: 2.75})
+//gsap.fromTo(".videoPlayer", {opacity: 0},{opacity: 1, duration: 1.75})
 
 
